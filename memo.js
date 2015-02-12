5b6b9eb27f0e14f0556b29efe9c13bd450c2219b
takk0 <dominik.begere@gmail.com>
2015-02-12 09:48:08 +0100

    Update memo.js
    Stand: 12.02.2015

--- memo.js
+++ memo.js
@@ -1,11 +1,64 @@
-window.onload=init;
-	function init (){
-
-pic = document.getElementById('td1');
-pic.onclick = changeImg;	
-}
-
-function changeImg(){
-pic.src ='old_paper.png';
-}
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Hjördes Frese, MN: s0549328">
+        <meta name="author" content="Sandra Samol, MN: s0549899">
+        <meta name="author" content="Nadja Müller s0549481">
+        <meta name="author" content="Dominik Begere s0550254">
+        <meta name="author" content="Iuliia Bondarieva s0549166">
+        <title>PixelHistory</title>
+        <script src="memo.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="gestaltung.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="print.css" type="text/css" media="print">
+        <script src=“unserscript.js“ type=“text/javascript“>
+        </script>
+    </head>
+    <body>
+    
+    <!-- Menü-Kasten -->
+  <header>
+      <img id="logo" src="http://oi58.tinypic.com/2lxttw1.jpg" alt="Logo" title="Unser Logo">      
+          <h1 class="title">PixelHistory</h1>
+   <nav>
+      <ul>
+        <li><a href="Index.html">Index</a></li>
+        <li><a href="UeberUns.html">&Uumlber uns</a></li>
+        <li><a href="Memory.html">Memory</a></li>
+        <li><a href="Links.html">Links</a></li>
+      </ul>
+  </nav>
+  </header>
+  <body>
+  <table>
+   <tr>
+       <td> <img id="td1" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td9" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td3" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td4a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td5a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+   </tr>
+   <tr>
+       <td> <img id="td3a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td7" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td8a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td2" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td7a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+   </tr>
+   <tr>
+       <td> <img id="td6a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td8" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td1a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td5" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td10" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+   </tr>
+   <tr>
+       <td> <img id="td9a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td10a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td2a" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td6" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+       <td> <img id="td4" src="http://oi62.tinypic.com/dlnbti.jpg" alt="Rueckseite" title="Memory Rueckseite"> </td>
+   </tr>
+  </table>
+    </body>
+</html>
